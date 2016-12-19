038b120a7c1f2a264a96b1b94e8e7fd23f2da068
frogman <frogman20xx@gmail.com>
2016-12-19 04:00:16 +0100

    klasika

--- Pig_latin_code_academy.py
+++ Pig_latin_code_academy.py
@@ -1,8 +1,14 @@
 print('Welcome to the Pig Latin Translator!')
 
-# Start coding here!
-original = input("Enter a word:")
-if len(original) and original.isalpha() > 0:
+pyg = 'ay'
+
+original = input('Enter a word:')
+
+if len(original) > 0 and original.isalpha():
     print(original)
 else:
-    print("Empty")
+    print('empty')
+word = original.lower()
+first = word[0]
+new_word = word + first + pyg
+new_word = new_word[1:len(new_word)]
