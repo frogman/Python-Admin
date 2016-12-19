632047c023fbc8738efd8036e6f2e49275598f15
frogman <frogman20xx@gmail.com>
2016-12-20 00:42:08 +0100

    klasika

--- moduli.py
+++ moduli.py
@@ -16,3 +16,23 @@ print(math.sqrt(25))
 import math            # Imports the math module
 everything = dir(math) # Sets everything to a list of things from math
 print(everything)       # Prints 'em all!
+
+
+def biggest_number(*args): #zvjezdicom mozemo vise argumenata u nizu dodjeliti pozivanjem funkcije
+    print(max( args ))
+    return max( args )
+
+
+def smallest_number(*args):
+    print(min( args ))
+    return min( args )
+
+
+def distance_from_zero(arg):
+    print(abs( arg ))
+    return abs( arg )
+
+
+biggest_number( -10, -5, 5, 10 )
+smallest_number( -10, -5, 5, 10 )
+distance_from_zero( -10 )
