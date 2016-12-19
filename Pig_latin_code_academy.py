92bc5b58b24142511dae5609014014cb9952e358
frogman <frogman20xx@gmail.com>
2016-12-19 04:01:38 +0100

    klasika

--- Pig_latin_code_academy.py
+++ Pig_latin_code_academy.py
@@ -11,4 +11,5 @@ else:
 word = original.lower()
 first = word[0]
 new_word = word + first + pyg
-new_word = new_word[1:len(new_word)]
+new_word = new_word[1:len(new_word)] #slicing of string from 1 until end of complete string
+
