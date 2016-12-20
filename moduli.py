08f7953cf7fa2386b3c74069b02321aae7ff12ce
frogman <frogman20xx@gmail.com>
2016-12-20 01:10:35 +0100

    klasika

--- moduli.py
+++ moduli.py
@@ -55,3 +55,17 @@ print(type(42))
 print(type(4.2))
 print(type('spam'))
 
+#First, def a function, shut_down, that takes one argument s. Don't forget the parentheses or the colon!
+#Then, if the shut_down function receives an s equal to "yes", it should return "Shutting down"
+#Alternatively, elif s is equal to "no", then the function should return "Shutdown aborted".
+#Finally, if shut_down gets anything other than those inputs, the function should return "Sorry"
+
+
+def shut_down(s):
+    if s == 'yes':
+       return "Shutting down"
+    elif s == "no":
+       return "Shutdown Aborted"
+    else:
+       return "Sorry"
+
