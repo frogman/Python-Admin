eade7b344b7a6d6de001de7ceaab0870aa22087b
frogman <frogman20xx@gmail.com>
2016-12-20 01:11:16 +0100

    klasika

--- funkcije1.py
+++ funkcije1.py
@@ -62,3 +62,17 @@ def by_three(number):
     else:
         return False
 print(by_three( 3 ))
+
+#First, def a function, shut_down, that takes one argument s. Don't forget the parentheses or the colon!
+#Then, if the shut_down function receives an s equal to "yes", it should return "Shutting down"
+#Alternatively, elif s is equal to "no", then the function should return "Shutdown aborted".
+#Finally, if shut_down gets anything other than those inputs, the function should return "Sorry"
+
+
+def shut_down(s):
+    if s == 'yes':
+       return "Shutting down"
+    elif s == "no":
+       return "Shutdown Aborted"
+    else:
+       return "Sorry"
