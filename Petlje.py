46b11a7cd3b035456d37f65f910ed209669bc661
frogman <frogman20xx@gmail.com>
2016-12-21 17:27:02 +0100

    klasika

---
+++ Petlje.py
@@ -0,0 +1,5 @@
+#Running through all the items in a list is called traversing the list, or traversal.
+#for each item in a list run the foor lpp
+for f in ["Joe", "Zoe", "Brad", "Angelina", "Zuki", "Thandi", "Paris"]:
+    invitation = "Hi " + f + ".  Please come to my party on Saturday!"
+    print(invitation)
