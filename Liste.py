cd4dd174b2ad1800f5f5b7d0b34e03c80ca438d9
frogman <frogman20xx@gmail.com>
2016-12-21 17:46:14 +0100

    klasika

---
+++ Liste.py
@@ -0,0 +1,12 @@
+#You can access an individual item on the list by its index.
+# An index is like an address that identifies the item's place in the list.
+numbers = [5, 6, 7, 8]
+
+print("Adding the numbers at indices 0 and 2...")
+print(numbers[0] + numbers[2])
+print("Adding the numbers at indices 1 and 3...")
+
+#List indices begin with 0, not 1! You access the first item in a list like this: list_name[0].
+#INDICES -INDEXI
+print(numbers[1] + numbers[3])
+
--- Petlje.py
+++ Petlje.py
@@ -1,8 +1,8 @@
 #Running through all the items in a list is called traversing the list, or traversal.
-#for each item in a list run the foor lpp
-#for f in ["Joe", "Zoe", "Brad", "Angelina", "Zuki", "Thandi", "Paris"]:
-#    invitation = "Hi " + f + ".  Please come to my party on Saturday!"
-#    print(invitation)
+for each item in a list run the foor lpp
+for f in ["Joe", "Zoe", "Brad", "Angelina", "Zuki", "Thandi", "Paris"]:
+    invitation = "Hi " + f + ".  Please come to my party on Saturday!"
+    print(invitation)
 
 #Let us write a function now to sum up all the elements in a list of numbers.
 def mysum(xs):
@@ -17,3 +17,5 @@ print(mysum([1.25, 2.5, 1.75]) == 5.5) # suma brojeva u listi je jednaka == broj
 print(mysum([1, -2, 3]) == 2)
 print(mysum([ ]) == 0)
 print(mysum(range(11)) == 55)  # 11 is not included in the list.
+
+#while statement
